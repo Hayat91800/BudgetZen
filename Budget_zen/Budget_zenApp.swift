1a9632f15f5d8039ad2c28e9b58bd4ba13a88138
Hayat <hayatzakhoukh@gmail.com>
2026-06-30 13:40:56 +0200

    Initial Commit

---
+++ Budget_zen/Budget_zenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_zenApp.swift
+//  Budget_zen
+//
+//  Created by apprenant144 on 30/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Budget_zenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budget_zen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budget_zen
+//
+//  Created by apprenant144 on 30/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
