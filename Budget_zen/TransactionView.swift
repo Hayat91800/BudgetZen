f13efaf3be18c8576ff65d38a07431ffa123e051
Hayat <hayatzakhoukh@gmail.com>
2026-07-02 11:24:08 +0200

    Features 3 - Update filter and searchbar

--- Budget_zen/TransactionView.swift
+++ Budget_zen/TransactionView.swift
@@ -13,26 +13,53 @@ struct TransactionView: View {
     
     @State private var ajout = false
     @State private var research = ""
+    @State private var selectedCategory: Category = .Alimentation
+    @State private var selectedFilter = 0
     
     var onAjout: (Transaction) -> Void = { _ in }
     
+    private var filteredCategories: [Category] {
+        Category.allCases.filter {$0.revenu == (selectedFilter == 2)}
+    }
+    
     var results: [Transaction] {
+        let typeFiltered = vm.arrOfAllTransaction.filter { transaction in
+            switch selectedFilter {
+            case 1: return transaction.amount < 0 // Dépense
+            case 2: return transaction.amount > 0 // Revenu
+            default: return true // Toutes les transactions
+            }
+        }
+    
         if research.isEmpty {
-            return vm.arrOfAllTransaction
+            return typeFiltered
         } else {
-            return vm.arrOfAllTransaction.filter { transaction in
+            return typeFiltered.filter { transaction in
                 transaction.title.localizedCaseInsensitiveContains(research)
                 // localizedCaseInsensitiveContains: insensible a la casse
             }
         }
-    }
+}
     
     var body: some View {
-
+        
         NavigationStack{
-    
-            List {
-                    ForEach(results) {transaction in
+            VStack (spacing: 0){
+                Section {
+                    Picker("Type de transaction", selection: $selectedFilter){
+                        Text("Tout").tag(0)
+                        Text("Dépense").tag(1)
+                        Text("Renevu").tag(2)
+                    }
+                    .pickerStyle(.segmented)
+                    .padding()
+                    .background(Color(.systemGroupedBackground))
+                    .onChange(of: selectedFilter) { _, newValue in
+                        selectedCategory = .Alimentation}
+                }
+                
+                List {
+                    ForEach(results) { transaction in
                         
                         HStack(spacing: 24) {
                             
@@ -43,10 +70,10 @@ struct TransactionView: View {
                                 .foregroundStyle(transaction.category.color)
                             
                             VStack(alignment: .leading) {
-
+                                
                                 Text(transaction.title)
-                                .font(.title3)
-                                .foregroundStyle(.black)
+                                    .font(.title3)
+                                    .foregroundStyle(.black)
                                 
                                 Text(transaction.category.rawValue)
                             }
@@ -58,15 +85,13 @@ struct TransactionView: View {
                             
                         }
                         .alignmentGuide(.listRowSeparatorLeading) { _ in
-                            return 0 // Aligne le separateur sur le bord avant
-                        }
+                            return 0 } // Aligne le separateur sur le bord avant 
                     }
-
                 }
+            }
                 .navigationTitle("Transaction")
-                .searchable(text: $research, prompt:"Rechercher une transaction")
+                .searchable(text: $research, placement: .navigationBarDrawer(displayMode: .always), prompt:"Rechercher une transaction")
                 .textInputAutocapitalization(.never) // Empêche la majuscule automatique
-                
                 .toolbar {
                     ToolbarItem (placement: .topBarTrailing) {
                         Button {
@@ -85,7 +110,7 @@ struct TransactionView: View {
                 }
             }
         }
-}
+    }
 
 #Preview {
     TransactionView()
